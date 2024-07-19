36ce88b4b1f37e6996382bef0ae7f2d214703e34
Ajmeri Gudiya <149288719+Ajmeri799@users.noreply.github.com>
2024-07-19 23:36:01 +0530

    Update profile.jsx

--- src/profile.jsx
+++ src/profile.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useState } from "react";
 function Profile() {
+  
   const [profile, setProfile] = useState("");
   const [data, setData] = useState("");
   const [repo, setrepo] = useState([]);
